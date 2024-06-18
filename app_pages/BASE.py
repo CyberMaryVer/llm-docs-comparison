5ee4ee14707091a24a8ac18d0ce966f00b09a365
CyberMaryVer <maria.s.startseva@gmail.com>
2024-06-18 04:07:16 +0300

    readme

--- app_pages/BASE.py
+++ app_pages/BASE.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import streamlit_shadcn_ui as ui
-
+import pandas as pd
 import os
 
 from app_utils.base_comparison import pipeline
+from app_utils.general import merge_final
 
 
 def save_files_in_temp_dir(files):
@@ -20,26 +21,51 @@ def main():
     doc1 = None
     doc2 = None
 
-    files = st.file_uploader("Upload two documents", type=["docx", ], accept_multiple_files=True)
-    api_key = st.text_input("Enter your OpenAI API key")
-    llm_model = st.selectbox("Select LLM model", ["gpt-4o", "gpt-3.5-turbo"])
+    col1, col2 = st.columns((4, 2))
 
-    if files:
-        # Check if two files are uploaded
-        if len(files) != 2:
-            # Show select two files message
-            st.warning("Please select two files to compare")
-        else:
-            doc1, doc2 = save_files_in_temp_dir(files)
-            st.session_state["files"] = [doc1, doc2]
+    with col1:
+        files = st.file_uploader("Upload two documents", type=["docx", ], accept_multiple_files=True)
+        api_key = st.text_input("Enter your OpenAI API key")
+        llm_model = st.selectbox("Select LLM model", ["gpt-4o", "gpt-3.5-turbo"])
 
-    compare_button = ui.button(text="Compare",
-                               variant="primary",
-                               class_name="bg-indigo-600 hover:bg-cyan-600",
-                               key="compare_button")
-    if 'files' in st.session_state and compare_button:
-        with st.spinner("Comparing documents... It may take a while... (up to 30 minutes)"):
-            result = pipeline(doc1, doc2, api_key, llm_model, save_intermediate_steps=True)
+        if files:
+            # Check if two files are uploaded
+            if len(files) != 2:
+                # Show select two files message
+                st.warning("Please select two files to compare")
+            else:
+                doc1, doc2 = save_files_in_temp_dir(files)
+                st.session_state["files"] = [doc1, doc2]
 
-        # Show the result
-        st.write(result)
+        compare_button = ui.button(text="Compare",
+                                   variant="primary",
+                                   class_name="bg-indigo-600 hover:bg-cyan-600",
+                                   key="compare_button")
+        if 'files' in st.session_state and compare_button:
+            with st.spinner("Comparing documents... It may take a while... (up to 30 minutes)"):
+                result = pipeline(doc1, doc2, api_key, llm_model, save_intermediate_steps=True)
+                st.session_state["result"] = result
+
+        if 'result' in st.session_state:
+            df_data, missed_ids1 = merge_final(result)
+            df = pd.DataFrame(df_data)
+
+            # Show the result
+            st.write(df)
+
+    with col2:
+        st.write("This is the base approach for comparing two documents. "
+                 "The documents are compared using the LLM model from OpenAI. "
+                 "The documents are split into pages and the LLM model is used to summarize the pages. "
+                 "The summaries are then compared using the LLM model to generate a final comparison result. "
+                 "The final result is displayed as a table showing the comparison between the two documents.")
+
+        if 'result' in st.session_state:
+            # try to open the intermediate steps
+            try:
+                st.write("Intermediate steps:")
+                for i in range(1, 4):
+                    with open(f"temp/base_step_{i}.txt", "r") as f:
+                        st.write(f.read())
+            except FileNotFoundError:
+                st.write("Intermediate steps not found")
